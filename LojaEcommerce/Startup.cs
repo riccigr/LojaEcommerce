97c9c3ab4978eaa414fd99d121fafdf3f01a6631
riccigr <ricci.gr@gmail.com>
2017-09-16 19:04:52 -0300

    implementando navegacao

--- LojaEcommerce/Startup.cs
+++ LojaEcommerce/Startup.cs
@@ -53,7 +53,7 @@ namespace LojaEcommerce
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
         }
     }
